0dd1c4650d8fde7b8af7014fe53538a74805d2b5
Hendrik Boerma <hendrikboerma@hendriks-mbp-5.home>
2023-08-04 18:07:34 +0200

    next race feature

--- main.js
+++ main.js
@@ -1,37 +1,64 @@
-fetch("https://ergast.com/api/f1/2023/driverStandings")
+function fetchAndProcessData(url, processDataCallback) {
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, "application/xml");
+      processDataCallback(xml);
+    })
+    .catch(console.error);
+}
+
+function updateDriverStandings(xml) {
+  let text = "";
+  for (let i = 0; i < 21; i++) {
+    text += `<tr><td>${i + 1}</td><td>${xml.querySelectorAll('GivenName')[i].textContent} ${xml.querySelectorAll('FamilyName')[i].textContent}</td><td>${xml.querySelectorAll('DriverStanding')[i].getAttribute('points')}</td></tr>`;
+  }
+  const standingsListElement = xml.querySelector("StandingsList");
+  const round = `<span>Season: ${standingsListElement.getAttribute('season')}</span><span>Round: ${standingsListElement.getAttribute('round')}</span>`;
+  document.getElementById("driverlist").innerHTML = text;
+  document.getElementById("round").innerHTML = round;
+  console.log(xml)
+}
+
+function updateConstructorStandings(xml) {
+  let text = "";
+  for (let i = 0; i < 10; i++) {
+    text += `<tr><td>${i + 1}</td><td>${xml.querySelectorAll('Name')[i].textContent}</td><td>${xml.querySelectorAll('ConstructorStanding')[i].getAttribute('points')}</td></tr>`;
+  }
+  document.getElementById("constructorlist").innerHTML = text;
+  console.log(xml)
+}
+
+function updateCurrentRound(xml) {
+  let listelement = xml.querySelector("StandingsList");
+  let currentRound = listelement.getAttribute('round');
+  updateNextRace(currentRound);
+}
+
+function updateNextRace(i) {
+  fetch("https://ergast.com/api/f1/current")
   .then(response => response.text())
   .then(data => {
     const parser = new DOMParser();
     const xml = parser.parseFromString(data, "application/xml");
     console.log(xml);
 
-    let text = "";
-    for (let i=0; i < 21; i++){
-        text += "<tr><td>" + (i + 1) + "</td><td>" + xml.querySelectorAll('GivenName')[i].textContent + " " + xml.querySelectorAll('FamilyName')[i].textContent + "</td><td>" + xml.querySelectorAll('DriverStanding')[i].getAttribute('points') + "</td></tr>"; 
-    }
-    let standingsListElement = xml.querySelector("StandingsList");
-    let round = "<span>" + "Season: " + standingsListElement.getAttribute('season') + "</span>" + "<span>" + " Round: " + standingsListElement.getAttribute('round') + "</span>";
-    document.getElementById("driverlist").innerHTML = text;
-    document.getElementById("round").innerHTML = round;
-  })
-  .catch(console.error);
-
-
-  fetch("https://ergast.com/api/f1/2023/constructorStandings")
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(data, "application/xml");
-    console.log(xml);
-
-    let text = "";
-    for (let i=0; i < 10; i++){
-        text += "<tr><td>" + (i + 1) + "</td><td>" + xml.querySelectorAll('Name')[i].textContent + "</td><td>" + xml.querySelectorAll('ConstructorStanding')[i].getAttribute('points') + "</td></tr>"; 
-    }
-    document.getElementById("constructorlist").innerHTML = text;
-  })
-  .catch(console.error);
-
+    const race = xml.querySelectorAll("Race")[i];
+    console.log(race);
+    const date = race.querySelectorAll("Date")[0].textContent;
+    const inputValue = race.querySelectorAll("Time")[0].textContent.replace(":00Z", "");
+    const [hours, minutes] = inputValue.split(":").map(Number);
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const time = (hours + 2 + ":" + formattedMinutes);
+    const racename = xml.querySelectorAll("RaceName")[i].textContent;
+    const location = xml.querySelectorAll("Country")[i].textContent;
+    document.getElementById("nextrace").innerHTML = racename;
+    document.getElementById("date").innerHTML = date;
+    document.getElementById("time").innerHTML = time;
+    document.getElementById("location").innerHTML = location;
+})
+}
 
   function toggleList(listType) {
     const constructorStandings = document.getElementById('constructors');
@@ -53,3 +80,7 @@ fetch("https://ergast.com/api/f1/2023/driverStandings")
       constructorTab.classList.remove('active');
     }
   }
+
+fetchAndProcessData("https://ergast.com/api/f1/current/driverStandings", updateDriverStandings);
+fetchAndProcessData("https://ergast.com/api/f1/current/constructorStandings", updateConstructorStandings);
+fetchAndProcessData("https://ergast.com/api/f1/current/driverStandings", updateCurrentRound);
