85468528675d23ab36e464090e58fdd5c2814da0
Hendrik Boerma <hendrikboerma@gmail.com>
2024-03-08 10:27:02 +0100

    summerwintertime

--- main.js
+++ main.js
@@ -34,62 +34,80 @@ function updateCurrentRound(xml) {
   updateNextRace(currentRound);
 }
 
+function lastSunday(month) {
+  const currentYear = new Date().getFullYear();
+  const date = new Date(currentYear, month, 1, 12);
+  let weekday = date.getDay();
+  let dayDiff = weekday === 0 ? 7 : weekday;
+  let lastSunday = date.setDate(date.getDate() - dayDiff);
+  return new Date(lastSunday).toDateString();
+}
+
 function updateNextRace(i) {
   fetch("https://ergast.com/api/f1/current")
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(data, "application/xml");
-    const race = xml.querySelectorAll("Race")[i];
+    .then(response => response.text())
+    .then(data => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, "application/xml");
+      const race = xml.querySelectorAll("Race")[i];
 
-    if (race === undefined) {
-      document.getElementById("nextrace").innerHTML = 'End of season';
-      document.getElementById("coma").innerHTML = '';
-    } else {
-    const year = new Date().getFullYear();
-    const inputDate = race.querySelectorAll("Date")[0].textContent.replace(`${year}-`, "");
-    const [month, day] = inputDate.split('-'); 
-    const monthNames = [
-      "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-    ];
-    const abbreviatedMonth = monthNames[parseInt(month) - 1];
-    const date = `${abbreviatedMonth} ${day}`;
-    const inputValue = race.querySelectorAll("Time")[0].textContent.replace(":00Z", "");
-    const [hours, minutes] = inputValue.split(":").map(Number);
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    const time = (hours + 1 + ":" + formattedMinutes);
-    const racename = xml.querySelectorAll("RaceName")[i].textContent;
-    const location = xml.querySelectorAll("Country")[i].textContent;
-    document.getElementById("nextrace").innerHTML = racename;
-    document.getElementById("date").innerHTML = date;
-    document.getElementById("time").innerHTML = time;
-    document.getElementById("location").innerHTML = location;
-  }
-})
+      if (race === undefined) {
+        document.getElementById("nextrace").innerHTML = 'End of season';
+        document.getElementById("coma").innerHTML = '';
+      } else {
+        const year = new Date().getFullYear();
+        const inputDate = race.querySelectorAll("Date")[0].textContent.replace(`${year}-`, "");
+        const [month, day] = inputDate.split('-');
+        const monthNames = [
+          "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+        ];
+        const abbreviatedMonth = monthNames[parseInt(month) - 1];
+        const date = `${abbreviatedMonth} ${day}`;
+        const raceDate = new Date(race.querySelectorAll("Date")[0].textContent.replace())
+        const inputValue = race.querySelectorAll("Time")[0].textContent.replace(":00Z", "");
+        const [hours, minutes] = inputValue.split(":").map(Number);
+        const formattedMinutes = String(minutes).padStart(2, "0");
+        const currentDate = new Date();
+
+        if (raceDate > new Date(lastSunday(3)) && raceDate < new Date(lastSunday(10))) {
+          const time = (hours + 2 + ":" + formattedMinutes);
+          document.getElementById("time").innerHTML = time;
+        } else {
+          const time = (hours + 1 + ":" + formattedMinutes);
+          document.getElementById("time").innerHTML = time;
+        }
+
+        const racename = xml.querySelectorAll("RaceName")[i].textContent;
+        const location = xml.querySelectorAll("Country")[i].textContent;
+        document.getElementById("nextrace").innerHTML = racename;
+        document.getElementById("date").innerHTML = date;
+        document.getElementById("location").innerHTML = location;
+      }
+    })
 }
 
 
-  function toggleList(listType) {
-    const constructorStandings = document.getElementById('constructors');
-    const driverStandings = document.getElementById('drivers');
-    const driverTab = document.getElementById('drivertab');
-    const constructorTab = document.getElementById('constructortab');
-  
-    if (listType === 'constructor') {
-      constructorStandings.style.display = 'block';
-      driverStandings.style.display = 'none';
+function toggleList(listType) {
+  const constructorStandings = document.getElementById('constructors');
+  const driverStandings = document.getElementById('drivers');
+  const driverTab = document.getElementById('drivertab');
+  const constructorTab = document.getElementById('constructortab');
 
-      driverTab.classList.remove('active');
-      constructorTab.classList.add('active');
-    } else if (listType === 'driver') {
-      constructorStandings.style.display = 'none';
-      driverStandings.style.display = 'block';
-      
-      driverTab.classList.add('active');
-      constructorTab.classList.remove('active');
-    }
+  if (listType === 'constructor') {
+    constructorStandings.style.display = 'block';
+    driverStandings.style.display = 'none';
+
+    driverTab.classList.remove('active');
+    constructorTab.classList.add('active');
+  } else if (listType === 'driver') {
+    constructorStandings.style.display = 'none';
+    driverStandings.style.display = 'block';
+
+    driverTab.classList.add('active');
+    constructorTab.classList.remove('active');
   }
+}
 
 fetchAndProcessData("https://ergast.com/api/f1/current/driverStandings", updateDriverStandings);
 fetchAndProcessData("https://ergast.com/api/f1/current/constructorStandings", updateConstructorStandings);
